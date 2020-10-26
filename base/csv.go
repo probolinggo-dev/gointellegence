97802a103cfa2f337e618fdcbbace0f2738b69f1
mygetzu <mbenypangestu@gmail.com>
2020-10-26 15:43:24 +0700

    feat: 🎸 base (csv,json) input

--- base/csv.go
+++ base/csv.go
@@ -2,12 +2,13 @@ package base
 
 import (
 	"github.com/go-gota/gota/dataframe"
+	"strings"
 )
 
-func readCsv() dataframe.Dataframe {
-
+func readCsv(csvStr string) dataframe.DataFrame {
+	df := dataframe.ReadCSV(strings.NewReader(csvStr))
+	return df
 }
 
-func exportToCsv() dataframe.Dataframe {
-
+func exportToCsv()  {
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/probolinggo-dev/gointellegence
 
 go 1.13
+
+require (
+	github.com/go-gota/gota v0.10.1
+	gonum.org/v1/gonum v0.8.1 // indirect
+)
